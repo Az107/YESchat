160929f8b9be567419b3c7650078c9571f89650c
Alberto Ruiz <17555470+Az107@users.noreply.github.com>
2023-06-01 21:47:13 +0200

    Initial Commit

---
+++ YES/YESApp.swift
@@ -0,0 +1,17 @@
+//
+//  YESApp.swift
+//  YES
+//
+//  Created by Alberto Ruiz on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct YESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
